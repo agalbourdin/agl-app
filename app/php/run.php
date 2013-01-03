d0cc61e46cab43c5981e0e255405c1f16d1ead9b
Alexis Galbourdin <i@alexisg.net>
2013-01-03 15:47:11 +0100

    Path to AGL updated.

--- app/php/run.php
+++ app/php/run.php
@@ -1,8 +1,19 @@
 <?php
 /**
  * Required constants to initialize AGL.
+ *
+ * AGL_PATH (string): absolute or relative path to your AGL installation.
+ *
+ * AGL_CACHE_ENABLED (bool): enable the Cache. Configuration files will
+ * automatically be cached. Views and Blocks will be cached if configured.
+ * Cache is saved to memory if APC is available, or to file system
+ * (in app/var/cache, app/var must be writable).
+ *
+ * AGL_DEBUG_MODE (bool): enable the Debug mode. PHP errors and exceptions will
+ * be displayed. Debug informations will be displayed at the bottom of the
+ * pages.
  */
-define('AGL_PATH', '/data/Sites/Alexis/agl_git/agl/');
+define('AGL_PATH', '/usr/local/lib/agl/');
 define('AGL_CACHE_ENABLED', false);
 define('AGL_DEBUG_MODE', false);
 
